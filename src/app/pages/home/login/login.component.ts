b0b620055f82af4e82a1715d5bcdae4583d878f3
kans_kant <kanssougiba627@gmail.com>
2021-07-20 11:03:54 +0100

    update of login as admin

--- src/app/pages/home/login/login.component.ts
+++ src/app/pages/home/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TokenStorageService } from 'src/app/services/token-storage.service';
 import { WebSocketService } from 'src/app/services/websocket.service';
+import Swal from 'sweetalert2';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -12,38 +13,44 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm : FormGroup;
-  credentials = {'email': '','password':''}
+  loginForm: FormGroup;
+  credentials = { 'email': '', 'password': '' }
 
   constructor(
-    private authService : AuthService,
-    private tokenStorage : TokenStorageService,
-    private router : Router,
-    private wsSocket : WebSocketService
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService,
+    private router: Router,
+    private wsSocket: WebSocketService
   ) { }
 
   ngOnInit(): void {
     this.initForm();
   }
 
-  initForm(){
+  initForm() {
     this.loginForm = new FormGroup({
-      'email' : new FormControl('',[Validators.required]),
-      'password' : new FormControl('',[Validators.required])
+      'email': new FormControl('', [Validators.required]),
+      'password': new FormControl('', [Validators.required])
     })
   }
 
-  onLogin(){
+  onLogin() {
     this.credentials.email = this.loginForm.value.email;
     this.credentials.password = this.loginForm.value.password;
     this.authService.login(this.credentials).subscribe(
-      (data)=>{
-        this.tokenStorage.saveToken(data.token);
-        this.tokenStorage.saveUser(data.currentUser);
-        this.wsSocket.connect();
-        console.log(data);
-        this.tokenStorage.saveRole(data.currentUser.authorities[0].authority);
-        this.router.navigate(['/admin']);
+      (data) => {
+        if (data.currentUser.authorities[0].authority==="ASSISTANTE") {
+          this.tokenStorage.saveToken(data.token);
+          this.tokenStorage.saveUser(data.currentUser);
+          this.wsSocket.connect();
+          this.tokenStorage.saveRole(data.currentUser.authorities[0].authority);
+          this.router.navigate(['/admin']);
+        }else{
+          Swal.fire({
+            'icon' : 'error',
+            'text' : 'You are not authorized'
+          })
+        }
       }
     )
   }
